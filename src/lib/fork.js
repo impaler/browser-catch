6f9d01b64de3d407a26311591cc690fce61fa7bd
Christopher Decoster <impaler777@gmail.com>
2017-01-17 20:34:05 +1100

    feat(fork) allow space separated command & reject on process error.

--- src/lib/fork.js
+++ src/lib/fork.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const execFile = require('child_process').execFile
+const util = require('util');
 
 const binPath = path.resolve(`${__dirname}/../../bin/browser-catch.js`)
 
@@ -7,10 +8,14 @@ export default async function (args) {
   return new Promise(onExec)
 
   function onExec (resolve, reject) {
+    if (util.isString(args)) {
+      args = args.split(' ')
+    }
+
     args = [binPath, ...args]
 
     execFile('node', args, (error, stdout, stderr) => {
-      if (!stdout || stdout === '') {
+      if (error || !stdout || stdout === '') {
         reject({error, stdout, stderr})
       }
       resolve({error, stdout, stderr})
