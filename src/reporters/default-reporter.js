a1f5b8aa08d86364b0d67130bb502b263745d8ec
Christopher Decoster <impaler777@gmail.com>
2017-04-09 15:23:33 +1000

    (refactor) fix reporters to accept catchUrls result objects.

--- src/reporters/default-reporter.js
+++ src/reporters/default-reporter.js
@@ -1,24 +1,22 @@
 const figures = require('figures')
 const colors = require('ansicolors')
 
-export function report (result) {
-  Array.isArray(result) ?
-    result.forEach(reportSingle) :
-    reportSingle(result)
-}
-
-function reportSingle (result) {
-  if (result.errors.length > 0) {
-    console.log(colors.white(JSON.stringify(result, null, 4)))
-
+export function report (errorCount) {
+  if (errorCount > 0) {
     console.error(colors.red(
-      figures(`✖ Error ${result.errors.length} console errors`)
+      figures(`✖ Error ${errorCount} console errors`)
     ))
   } else {
-    console.log(colors.blue(JSON.stringify(result, null, 4)))
-
     console.log(colors.green(
       figures('✔ Pass no console errors')
     ))
+  }
+}
+
+export function reportSingle (result) {
+  if (result.errors.length > 0) {
+    console.log(colors.white(JSON.stringify(result, null, 4)))
+  } else {
+    console.log(colors.blue(JSON.stringify(result, null, 4)))
   }
 }
